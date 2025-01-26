78411732c8119d62186e6d14101168bff1640835
Manish <keshraram294@gmail.com>
2025-01-26 20:05:38 +0530

    Some Changes

--- backend/services/DB/DB.js
+++ backend/services/DB/DB.js
@@ -5,6 +5,7 @@ export async function CONNECT(){
     try {
         console.log(process.env.MONGODB_URL)
         await mongoose.connect(process.env.MONGODB_URL)
+        console.clear()
         console.log('CONNECTED TO DB')
     } catch (error) {
         console.log(error)
--- backend/services/Redis.js
+++ backend/services/Redis.js
@@ -3,11 +3,15 @@ import Redis from 'ioredis';
 configDotenv()
 
 
-const RedisCLI = new Redis({
+const RedisOptions = {
     host:process.env.REDIS_HOST,
     port: process.env.REDIS_PORT,
     password: process.env.REDIS_PASSWORD,
-})
+}
+
+console.log(RedisOptions)
+
+const RedisCLI = new Redis(RedisOptions)
 
 RedisCLI.on('connect',() => {
     console.log('Connected to Redis');
